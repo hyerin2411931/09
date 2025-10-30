d56c8dc15edf84abf082e4e6f042025d9ddfebd0
hyerin2411931 <2411931@sookmyung.ac.kr>
2025-10-30 15:49:25 +0900

    Week09_03

--- main.c
+++ main.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5 
+#define SIZE 5  //전처리기. 매크로. SIZE라는 거를 전부 5로 바꿈. 
+
 
 int main(int argc, char *argv[])
 {
-  int i, average;
-  int sum=0;
-  int grade[SIZE];
+  int i;
+  int a[SIZE] = {1,2,3,4,5};
+  int b[SIZE] = {1,2,3,4,5};
+  int flag_same = 1;
   
-  for(i = 0; i < SIZE; i++){   // <=SIZE 아니고 <SIZE 여야 SIZE 개수만큼 반복문 돌음.  
-    printf("input grade:");
-    scanf("%d", &grade[i]);
-    sum += grade[i];
+  for(i = 0; i < SIZE; i++)
+  {
+    if(a[i] != b[i])
+    {
+      printf("배열은 다른  값을 가집니다.\n");
+      flag_same = 0;
     }
-  average = sum/SIZE;
-  printf("average = %d\n", average);  
+    else
+    printf("배열은 같은 값을 가집니다.\n"); 
+  } 
   
   
   system("PAUSE");	
